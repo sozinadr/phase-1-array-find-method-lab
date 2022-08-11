d59fe641dd1f8c37ba0fbd0f7bd68d01d6a85d0a
sozi <sozyarnadir@gmail.com>
2022-08-12 02:00:42 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+
+function superbowlWin(record) {
+    const recordResult = record.find((recordObj) => recordObj.result == "W");
+    if (recordResult === undefined) {
+        //Record results returns undefined if the record has no win objects'
+    } else {
+        return recordResult.year;
+    }
+}
